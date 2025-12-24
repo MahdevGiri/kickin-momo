0f1b61497c7115873ec95d57da1bcedba66baae4
MahdevGiri <mahesh.irig77@gmail.com>
2025-12-23 18:01:13 -0600

    removed the feedback section

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,11 +5,11 @@ import { About } from './components/about/about';
 import { Menu } from './components/menu/menu';
 import { Location } from './components/location/location';
 import { Footer } from './components/footer/footer';
-import { Feedback } from './components/feedback/feedback';
+
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero, About, Menu, Location, Feedback, Footer],
+  imports: [Header, Hero, About, Menu, Location, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
